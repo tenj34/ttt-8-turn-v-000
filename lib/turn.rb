368bc370aeff79c852bd014c4c3218120746863b
Juan Tenezaca <tenj34@gmail.com>
2018-06-13 00:59:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,5 +4,13 @@ def display_board(board)
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def valid_move?(board, index)
+  spot = index.to_i - 1
+  if !position_taken?(board,index) && index.between?(0,8)
+    true
+  else
+    false
+  end
 end
