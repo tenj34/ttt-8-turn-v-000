f829a91d0d30d4cb8453f2ef0d7f565ebddc0807
Juan Tenezaca <tenj34@gmail.com>
2018-06-13 00:46:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,13 @@
 def display_board(board)
-puts "#{board}"
-
-
+   print ('    |   |   ')
+   print ('' +board[0]+ '    | ' +board[1]+ '  |   ' +board[2] )
+   print ('    |   |   ')
+   print ('---------------')
+   print ('    |   |   ')
+   print ('' +board[3]+ '    | ' +board[4]+ '  | ' +board[5] )
+   print ('    |   |   ')
+   print('-----------------')
+   print ('    |   |   ')
+   print ('' +board[6]+ '    | ' +board[7]+ '  |' +board[8] )
+   print ('    |   |   ')
 end
