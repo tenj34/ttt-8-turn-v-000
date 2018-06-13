ecac0205dad7c24b8ef709a12ba93e270c788529
Juan Tenezaca <tenj34@gmail.com>
2018-06-13 01:41:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,20 +50,3 @@ def turn(board)
     end
   end
 end
-
-
-=begin
-boolCheck = false
-while boolCheck == false
-  input = gets.strip # convert value
-  location = input_to_index(input)
-
-  if valid_move?(board,location) == false #value check
-      puts "Not a valid choice. Please try again"
-    else
-      boolCheck = true
-      move(board,user_input,current_player(board))
-      display_board(board)
-    end
-  end
-=end
